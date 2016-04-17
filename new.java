dda02a6a013f2ff7d0fb578d80a46e7e0c163455
vishthite <vishalthite4444@gmail.com>
2016-04-17 11:15:01 +0530

    modified

--- new.java
+++ new.java
@@ -1,1 +1,16 @@
-fda
+import java.util.*;
+public class funClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String line = new String("Where");
+		//line = 
+		int len = line.length();
+		System.out.println(len);
+		//assertFalse();
+	}
+
+}
